d4ec57dd2c674061c8e8cd6b5e22d828bebc6ad3
uraway <masato.uraway@gmail.com>
2019-10-20 23:58:08 +0800

    test

--- src/app/Http/Controllers/Auth/VerificationController.php
+++ src/app/Http/Controllers/Auth/VerificationController.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Auth\VerifiesEmails;
 
 class VerificationController extends Controller
 {
+    use VerifiesEmails;
+
     /*
     |--------------------------------------------------------------------------
     | Email Verification Controller
@@ -18,7 +20,6 @@ class VerificationController extends Controller
     |
     */
 
-    use VerifiesEmails;
 
     /**
      * Where to redirect users after verification.
