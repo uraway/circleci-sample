540437a3edc38a4b18c1bda8124fa60b121bceb6
uraway <masato.uraway@gmail.com>
2019-10-25 00:07:32 +0800

    http://localhost:3000

--- screenshot.js
+++ screenshot.js
@@ -1,6 +1,6 @@
 var instance = require('webpage').create();
 
-instance.open('http://redpanthers.co', function () {
+instance.open('http://localhost:3000', function () {
   instance.render('screenshot.png');
   console.log('screenshot.png was saved!')
   phantom.exit();
