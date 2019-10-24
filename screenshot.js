8c29a0270ca4f2cc88993b00d5529d8434e1d106
uraway <masato.uraway@gmail.com>
2019-10-24 23:58:29 +0800

    use image

--- screenshot.js
+++ screenshot.js
@@ -1,6 +1,7 @@
 var instance = require('webpage').create();
 
 instance.open('http://redpanthers.co', function () {
-  instance.render('screenshot-phantom.png');
+  instance.render('screenshot.png');
+  console.log('screenshot.png was saved!')
   phantom.exit();
 });
