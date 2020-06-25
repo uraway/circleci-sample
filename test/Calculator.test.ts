2998342711fa8d0ad32e797238126f1c688147c2
Masato Urai <masato.uraway@gmail.com>
2020-06-25 23:04:07 +0800

    Update test

--- test/Calculator.test.ts
+++ test/Calculator.test.ts
@@ -4,6 +4,6 @@ import Calculator from "../src/Calculator";
 describe("calculate", function() {
   it("add", function() {
     const result = Calculator.sum([5, 2]);
-    expect(result).equal(7);
+    expect(result).equal(6);
   });
 });
